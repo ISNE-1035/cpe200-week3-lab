fea64e926a0ed03b2f034b6e3f354d5fa694948f
ISNE-1035 <poramin_s@cmu.ac.th>
2016-09-16 14:32:26 +0700

    Update BinaryCalculator.java

--- lab3_1/src/cpe200/BinaryCalculator.java
+++ lab3_1/src/cpe200/BinaryCalculator.java
@@ -1,51 +1,97 @@
 package cpe200;
 
-/**
- * Created by pruet on 5/9/2559.
- */
+
+import java.text.DecimalFormat;
 
 public class BinaryCalculator {
-    /* your code here */
+    private Operand firstOperand;
+    private Operand secondOperand;
 
-    public BinaryCalculator()
-    {
-        /* your code here */
+    public BinaryCalculator() {
     }
 
-    public void setFirstOperand(Operand operand)
-    {
-        /* your code here */
+    public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
     }
 
-
-    public void setSecondOperand(Operand operand)
-    {
-        /* your code here */
+    public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
     }
 
-    public String add()
-    {
-        /* your code here */
-        return null;
+    public String add() {
+        double total = Double.parseDouble(firstOperand.getOperand()) + Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            return Double.toString(total);
+        }
+
     }
 
-    public String subtract()
-    {
-        /* your code here */
-        return null;
+    public String subtract() {
+        double total = Double.parseDouble(firstOperand.getOperand()) - Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+            //Math.round(total,1);
+        }
+
     }
 
-    public String multiply()
-    {
-        /* your code here */
-        return null;
+    public String multiply() {
+        double total = Double.parseDouble(firstOperand.getOperand()) * Double.parseDouble(secondOperand.getOperand());
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+            //Math.round(total,1);
+        }
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division()
-    {
-        /* your code here */
-        return null;
+    public String division() throws ArithmeticException {
+        if(Double.parseDouble(secondOperand.getOperand()) != 0) {
+            double total = Double.parseDouble(firstOperand.getOperand()) / Double.parseDouble(secondOperand.getOperand());
+            if ((total == Math.floor(total) && !Double.isInfinite(total))) {
+                int i = (int) total;
+                return Integer.toString(i);
+
+            } else {
+                total = total*100000;
+                total = Math.floor(total);
+                int count = (int)total;
+                total = (double)count/100000;
+                return Double.toString(total);
+
+            }
+        }else{
+            int a=2;
+            return Integer.toString(a/0);
+        }
+
+    }
+
+    public String power() {
+        double firs,second;
+        double total;
+        firs = Double.parseDouble(firstOperand.getOperand());
+        second = Double.parseDouble(secondOperand.getOperand());
+        total = Math.pow(firs,second);
+        if( (total == Math.floor(total) && !Double.isInfinite(total))){
+            int i = (int)total;
+            return Integer.toString(i);
+        }else {
+            DecimalFormat formatter = new DecimalFormat("#0.0");
+            return formatter.format(total);
+            //Math.round(total,1);
+        }
+
     }
 
 
