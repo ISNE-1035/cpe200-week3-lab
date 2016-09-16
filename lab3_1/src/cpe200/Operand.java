fc8464cbfbeca9607f13d0122af3b98090a7a6f5
ISNE-1035 <poramin_s@cmu.ac.th>
2016-09-16 14:33:08 +0700

    Update Operand.java

--- lab3_1/src/cpe200/Operand.java
+++ lab3_1/src/cpe200/Operand.java
@@ -1,24 +1,17 @@
 package cpe200;
 
-/**
- * Created by pruet on 5/9/2559.
- */
 public class Operand {
-    String operand;
+    private String operand;
 
-    public Operand(String operand)
-    {
+    public Operand(String operand) {
         this.operand = operand;
     }
 
-    public Operand(int operand)
-    {
+    public Operand(int operand) {
         this.operand = Integer.toString(operand);
     }
 
-    public Operand(double operand)
-    {
-        /* your code here */
+    public Operand(double operand) {
+        this.operand = Double.toString(operand);
     }
-
 }
